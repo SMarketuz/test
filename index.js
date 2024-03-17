374ad6c4739a3fe12b71bdcd16c6c0711a3f8008
MOdevco <modevco05@gmail.com>
2024-03-17 21:35:38 +0500

    ffdsf

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
 
+mongoose.connect('mongodb+srv://modevco:1234modevco@atlascluster.bq01rxm.mongodb.net/modevco')
+.then(() => {
+    console.log('mongo ishladi');
+})
 
+const user = new mongoose.Schema({
+    name: String
+})
 
+const User = mongoose.model('User' , user)
 
-app.get('/' , (req, res) => {
-    res.send('Hello')
+app.post('/api/create' , async (req , res) => {
+    const userData = await User({
+        name: "salom"
+    })
+    const data = await userData.save()
+    console.log(data)
+})
+
+app.get('/' ,async (req, res) => {
+    const data = await User.find()
+    console.log(data);
 })
 
 
-
+ 
 
 
 const port = process.env.PORT || 8080;
